68a0ac2e198bba0543d5cdba1eb3da39f3b3296a
Terell-Davis <terellbdavis@gmail.com>
2023-04-09 19:22:06 -0400

    WIP

---
+++ src/main/java/project/conquest/galatic/modifyGui.java
@@ -0,0 +1,15 @@
+package project.conquest.galatic;
+
+import javax.swing.*;
+
+public class modifyGui extends JFrame {
+    public JPanel modifyPanel;
+    public JTextField MidR;
+    public JTextField BotL;
+    public JTextField BotR;
+    public JTextField MidL;
+    public JButton Submit;
+    public JTextField TopR;
+    public JTextField TopL;
+    public JTextField ModifyDisplay;
+}
