a44a3f1000c4f2a12919a3b0d0859726c94ff4f2
Terell-Davis <terellbdavis@gmail.com>
2023-04-09 19:20:51 -0400

    Wip

--- src/main/java/project/conquest/galatic/function.java
+++ src/main/java/project/conquest/galatic/function.java
@@ -7,31 +7,29 @@ import java.sql.SQLException;
 
 public class function {
     addPeopleGui people = new addPeopleGui(); static InfoGui info = new InfoGui();
-
-    JLabel fname = people.firstname, lname = people.lastname, topL = people.TopLeft, topR = people.TopRight,
+    // Will make this cleaner if time allows
+    /*JLabel fname = people.firstname, lname = people.lastname, topL = people.TopLeft, topR = people.TopRight,
             midL = people.MidLeft, midR = people.MidRight, botL = people.BottomLeft, botR = people.BottomRight;
 
+    JTextField fnametxt = people.fname, lnametxt = people.lname, topLtxt = people.TopLeftTxt, topRtxt = people.TopRightTxt,
+            midLtxt = people.MidLeftTxt, midRtxt = people.MidRightTxt, botLtxt = people.BottomLeftTxt, botRtxt = people.BottomRightTxt;
     public static void setCurrentTable(String x){
         info.currentTable = x;
     }
 
     public void set2Moff(){
-        // Visability of labels
-        people.lastname.setVisible(true); people.lname.setVisible(true);
-        people.BottomLeft.setVisible(false); people.BottomLeftTxt.setVisible(false);
-        people.BottomRight.setVisible(false); people.BottomRightTxt.setVisible(false);
+        // Visability of labels & text feilds
+        lname.setVisible(true); lnametxt.setVisible(true);
+        botL.setVisible(false); botLtxt.setVisible(false);
+        botR.setVisible(false); botLtxt.setVisible(false);
 
+        // Label Names
+        fname.setText("First Name:"); lname.setText("Last Name");
+        topL.setText("Title"); topR.setText("Id Number");
+        midL.setText("Home System"); midR.setText("Start Date");
+    }*/
 
-
-        //people.TopLeft.setText("StartDate");
-        people.TopRight.setText("Title");
-        people.MidLeft.setText("HomeSystem");
-        people.MidRight.setText("IdNumber");
-
-
-
-
-    }
+    //Something to make the line of code a little less long, pretty sure i can do this better but time limit
     public static Connection connectdb() {
         Connection conn;
         try {
