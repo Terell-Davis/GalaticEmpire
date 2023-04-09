87825ccc8f77ccd7b44b505b0d03ddddcf91c54f
Terell-Davis <terellbdavis@gmail.com>
2023-04-09 19:20:26 -0400

    Added query statements for each table repleaced if statement with a switch also updated clear button to clear names

--- src/main/java/project/conquest/galatic/addPeopleGui.java
+++ src/main/java/project/conquest/galatic/addPeopleGui.java
@@ -30,38 +30,73 @@ public class addPeopleGui extends JFrame {
     public JTextField TitleText;
     public JLabel TitleLabel;
     private JTextArea errorView;
-
-
+    String query = "";
     public addPeopleGui() {
     Connection conn = function.connectdb();
-    String TopL, TopR, MidL, MidR, BotR, BotL;
     OKbutton.addActionListener(new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
-
             String FName = fname.getText(), LName = lname.getText(),
             TopL = TopLeftTxt.getText(), TopR = TopRightTxt.getText(),
             MidL = MidLeftTxt.getText(), MidR = MidRightTxt.getText(),
             BotL = BottomLeftTxt.getText(), BotR = BottomRightTxt.getText();
+            String table= TitleText.getText();
 
-            String table = info.currentTable.toString();
+            System.out.println("The current table is: " + table);
+
+            switch (table) {
+                case "moffs":
+                    // INSERT INTO Moffs VALUES ('Sutton', 'Tion', '2021-03-04', 'Admiral', 'Yavin', '856839094');
+                    query = "INSERT INTO " + table + " VALUES ('" + FName + "', '" + LName + "', '" + MidR + "','"
+                            + TopL + "', '" + MidL + "', '" + TopR + "');";
+                    break;
+                case "campaigns":
+                    // INSERT INTO Campaigns VALUES ('Operation: Knightfall', '749275395', '198765432', 'Victory', '1975-03-07', '5378');
+                    query = "INSERT INTO " + table + " VALUES ('" + FName + "', '" + TopR + "', '" + LName + "','"
+                            + MidL + "', '" + MidR + "', '" + TopL + "');";
+                    break;
+                case "admirals":
+                case "generals":
+                    // INSERT INTO Admirals VALUES ('Zara', 'Krefayn', '381928372', '1981-06-08', '817654329', '4', '1950-04-23');
+                    query = "INSERT INTO " + table + " VALUES ('" + FName + "', '" + LName + "', '" + TopR + "','"
+                            + MidR + "', '" + TopL + "', '" + MidL + "','" + BotR + "');";
+                    break;
+                case "legions":
+                    // INSERT INTO Legions VALUES ('41st Elite Corps', '760473057', '143276589', '659278134', '3864', 'Jungle and Urban Warfare');
+                    query = "INSERT INTO " + table + " VALUES ('" + FName + "', '" + LName + "', '" + TopL + "','"
+                            + TopR + "', '" + MidR + "', '" + MidL + "');";
+                    break;
+                case "fleets":
+                    // INSERT INTO Fleets VALUES ('Tenth Fleet', '245789637', '587319462', 'Geonosis', '80');
+                    query = "INSERT INTO " + table + " VALUES ('" + FName + "', '" + LName + "', '" + TopL + "','"
+                            + TopR + "', '" + MidL + "');";
+                    break;
+                case "stormtroopers":
+                case "pilots":
+                    // INSERT INTO Stormtroopers VALUES ('CC-2224', 'Cody', '256478913', 'Kamino', '1979-11-08', 'Commander', '760473057');
+                    // INSERT INTO Pilots VALUES ('Kael', 'Valtor', '840172936', 'Ord Mantell', '2000-10-10', 'Captain', '58759650');
+                    query = "INSERT INTO " + table + " VALUES ('" + FName + "', '" + LName + "', '" + TopR + "','"
+                            + MidL + "', '" + MidR + "', '" + TopL + "', '" + BotR + "');";
+                    break;
+                case "base":
+                    // INSERT INTO Base VALUES ('Jedi Temple', '987126354', 'Coruscant', 'Destroyed', '0,10,20');
+                    query = "INSERT INTO " + table + " VALUES ('" + FName + "', '" + LName + "', '" + TopL + "','"
+                            + MidL + "', '" + TopR + "');";
+                    break;
+                case "systems":
+                    // INSERT INTO Systems VALUES ('Utapau', '385,358,90', '6', 'Controlled');
+                    query = "INSERT INTO " + table + " VALUES ('" + FName + "', '" + TopR + "', '" + TopL + "','"
+                            + MidL + "');";
+                    break;
+            }
+            System.out.println(query);
             try {
-                String query = "";
-                if(table == "moffs"){
-                    query = ("INSERT INTO " + table + " VALUES ('"+ FName +"', '" + LName +"', '"+ TopL +"','"
-                            + TopR +"', '"+ MidL +"', '"+ MidR +"');");
-                } else if (table == "admin") {
-
-                }
-
                 PreparedStatement stmt = conn.prepareStatement(query);
                 System.out.println(stmt.toString());
-                // INSERT INTO Moffs VALUES ('Sutton', 'Tion', '2021-03-04', 'Admiral', 'Yavin', '856839094');
 
                 int result = stmt.executeUpdate();
                 System.out.println(result + " Rows affected");
                 stmt.close();
-                conn.close();
             } catch (SQLException ex) {
                 ex.printStackTrace();
             }
@@ -70,6 +105,7 @@ public class addPeopleGui extends JFrame {
         ClearButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                fname.setText(""); lname.setText("");
                 TopLeftTxt.setText(""); TopRightTxt.setText("");
                 MidLeftTxt.setText(""); MidRightTxt.setText("");
                 BottomLeftTxt.setText(""); BottomRightTxt.setText("");
